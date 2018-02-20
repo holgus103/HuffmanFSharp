d56e112736fbb5dac211ab6583b5413de62cf1c5
holgus103 <kubasuchan@hotmail.com>
2018-02-21 00:15:44 +0100

    Travis test

--- Huffman/FrequencyTable.fs
+++ Huffman/FrequencyTable.fs
@@ -2,7 +2,9 @@ namespace Huffman
 
 module FrequencyTable = 
 
-    type MinInfo = {key : char; value : int}
+
+    type TreeNode = {left : TreeNode option; right : TreeNode option; value : int; key : char } 
+
     let incrementCounter (dict : Map<char, int>) (key : char) =
         match Map.tryFind key dict with
             | None -> Map.add key 1 dict
@@ -19,11 +21,45 @@ module FrequencyTable =
             
     let findMin counts = 
         counts
-        |> Map.toList
-        |> List.fold
-            (fun (state : MinInfo) x -> 
-                let (key, value) = x;
-                if state.value < 0 || value < state.value then {key = key; value = value}
-                else state
+        |> Map.fold
+            (fun ((key, state) : (char * TreeNode)) currentKey x -> 
+                if state.value < 0 || x.value < state.value then (currentKey, x)
+                else (key, state)
             )
-            {value = -1 ; key = '0'}
+            ('0', {value = -1 ; key = '0'; left = None; right = None})
+
+    let buildTree counts = 
+
+        let rec addNodes nodes =
+            if Map.count nodes = 1 then
+                nodes
+            else
+                let (min1key, min1val) =
+                    nodes
+                    |> findMin
+                
+                let (min2key, min2val) = 
+                    nodes
+                    |> Map.remove min1key
+                    |> findMin
+
+                nodes
+                |> Map.remove min1key
+                |> Map.remove min2key
+                |> Map.add min1key 
+                    {
+                        left = Some(min1val);
+                        right = Some(min2val);
+                        key = min1val.key;
+                        value = min1val.value + min2val.value
+                    }
+                |> addNodes
+            
+
+        let nodes = 
+            counts
+            |> Map.map (fun k x -> 
+                {left = None; right = None; value = x; key = k})
+
+        nodes 
+        |> addNodes
