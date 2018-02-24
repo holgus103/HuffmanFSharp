89997f60c3378bcfa0f4523ae08eb240b45a6ac1
holgus103 <kubasuchan@hotmail.com>
2018-02-24 03:04:23 +0100

    Added for long sequence of data

--- Tests/EncodingTests.fs
+++ Tests/EncodingTests.fs
@@ -16,7 +16,16 @@ type EncodingTests () =
 
         Trace.WriteLine(value)
         Assert.AreEqual(value.currentBuffer, 0b11001011)
-        // Assert.Fail()
+
+    [<TestMethod>]
+    member this.EncodingLongTest () =
+
+        let value = Main.encode "abccaabcabcabcabcabcabcabc"
+        01011    
+        Trace.WriteLine(value)
+        Assert.AreEqual(List.head value.output, 0b10110101101011010110101111001011)
+        Assert.AreEqual(value.currentBuffer, 0b01011010110)
+
 
 
 
