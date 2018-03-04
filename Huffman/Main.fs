548056013bc2e39923a41121393ae4acf77e8f0d
holgus103 <kubasuchan@hotmail.com>
2018-03-04 02:42:06 +0100

    Final draft - To be tested

--- Huffman/Main.fs
+++ Huffman/Main.fs
@@ -106,57 +106,83 @@ module Main =
             |> FrequencyTable.invertDictionary
             |> (fun x-> x.dict)
         
-        
+            
         let rec processCodes (state : DecodingState) =
 
-            let code =
-                match Map.tryFind state.foundCode dict with
-                // no code exists, extend current code
-                | None -> 
-                    {
-                        meaningfulBits = state.foundCode.meaningfulBits + 1;
-                        value =
-                            (
-                                1
-                                |> (-) state.bufferCount
-                                |> (<<<) 1
-                                |> (&&&) state.buffer,
-                                1
-                                |> (+) state.foundCode.meaningfulBits
-                                |> (-) state.bufferCount
-                            )                            
-                            ||> (>>>)
-                            |> (|||) state.buffer 
-                    }
-                // code found, reset current code
-                | Some x -> 
-                    {
-                        meaningfulBits = 1;
-                        value = 
-                            1
-                            |> (-) state.bufferCount
-                            |> (<<<) 1
-                            |> (&&&) state.buffer
-                            |> (>>>) (state.bufferCount - 1)
-                    }
-            if state.bufferCount = 0 then
-                // refill buffer
-                // TODO: everything
-                match state.input with
-                | [] -> state.output 
-                | b::tail -> 
-                    {
-                        input = tail;
-                        output = state.output;  
-                        buffer = b;
-                        bufferCount = 32;
-                        foundCode = {value = 0; meaningfulBits = 0}
 
-                        
-                    }
+
+            if state.bufferCount = 0 then
+            // no bits left to read, refill buffer and start another iteration
+                match state.input with
+                // end of input - exit condition
+                | [] -> 
+                    match Map.tryFind state.foundCode dict with 
+                    // fimished reading, just append the last character
+                    | Some x -> x::state.output
+                    // none found, input error?
+                    | None -> state.output
+                // refill buffer
+                | b::tail -> 
+                    match Map.tryFind state.foundCode dict with
+                    // append new characted and refill buffer
+                    | Some x ->
+                        {
+                            input = tail;
+                            output = x::state.output;  
+                            buffer = b;
+                            bufferCount = 32;
+                            foundCode = {value = 0; meaningfulBits = 0}                            
+                        }
+                    | None -> 
+                        {
+                            input = tail;
+                            output = state.output;
+                            buffer = b;
+                            bufferCount = 32;
+                            foundCode = state.foundCode;
+                        }
+                    // process further
                     |> processCodes
             else
-                List.empty
+                {
+                    input = state.input;
+                    output = state.output;
+                    buffer = state.buffer <<< 1;
+                    bufferCount = state.bufferCount - 1;
+                    foundCode = 
+                        match Map.tryFind state.foundCode dict with
+                        // no code exists, extend current code
+                        | None -> 
+                            {
+                                meaningfulBits = state.foundCode.meaningfulBits + 1;
+                                value =
+                                    (
+                                        1
+                                        |> (-) state.bufferCount
+                                        |> (<<<) 1
+                                        |> (&&&) state.buffer,
+                                        1
+                                        |> (+) state.foundCode.meaningfulBits
+                                        |> (-) state.bufferCount
+                                    )                            
+                                    ||> (>>>)
+                                    |> (|||) state.buffer 
+                            }
+                        // code found, reset current code
+                        | Some x -> 
+                            {
+                                meaningfulBits = 0;
+                                value = 
+                                    1
+                                    |> (-) state.bufferCount
+                                    |> (<<<) 1
+                                    |> (&&&) state.buffer
+                                    |> (>>>) (state.bufferCount - 1)
+                            }
+                
+                }
+                |> processCodes
+                
 
 
 
